06e4514e004f7e99e5768f760cba1d3a9ec53a27
Kayla Weaver <kaylabeth1211@gmail.com>
2024-01-01 16:53:12 -0800

    ticketcontrol and new ticketform

---
+++ src/components/TicketControl.js
@@ -0,0 +1,30 @@
+import React from "react";
+import NewTicketForm from "./NewTicketForm";
+import TicketList from "./TicketList";
+
+class TicketControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage: false
+    };
+  }
+  render(){
+    let currentlyVisibleState = null;
+    let addTicketButton = null;
+    if (this.state.formVisibleOnPage) {
+      currentlyVisibleState = <NewTicketForm />
+    } else {
+      currentlyVisibleState = <TicketList />
+      addTicketButton = <button onClick={this.handleClick}>Add Ticket</button>
+    }
+    return(
+      <React.Fragment>
+        {currentlyVisibleState}
+        {addTicketButton}
+      </React.Fragment>
+    );
+  }
+}
+
+export default TicketControl;
