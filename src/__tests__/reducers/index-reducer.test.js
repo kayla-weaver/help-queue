ed8ea842c08f17daf3e224aae18ff0a7992412c3
Kayla Weaver <kaylabeth1211@gmail.com>
2024-02-04 12:09:09 -0800

    red and green phase 2 and 3 indux reducer test

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -1,4 +1,9 @@
 import rootReducer from '../../reducers/index';
+import { createStore } from 'redux';
+import formVisibleReducer from '../../reducers/form-visible-reducer';
+import ticketListReducer from '../../reducers/ticket-list-reducer';
+
+let store = createStore(rootReducer);
 
 describe("rootReducer", () => {
 
@@ -9,4 +14,11 @@ describe("rootReducer", () => {
     });
   });
 
+  test('Check that initial state of ticketListReducer matches root reducer', () => {
+    expect(store.getState().mainTicketList).toEqual(ticketListReducer(undefined, { type: null }));
+  });
+  
+  test('Check that initial state of formVisibleReducer matches root reducer', () => {
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null }));
+  });
 });
