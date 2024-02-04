39ad47185d1ce0606c612dd28d64976985034586
Kayla Weaver <kaylabeth1211@gmail.com>
2024-02-04 12:58:27 -0800

    red phase action.js first test

---
+++ src/__tests__/actions/index.test.js
@@ -0,0 +1,10 @@
+import * as actions from './../../actions';
+
+describe('Help Queue actions', () => {
+  it('deleteTicket should create DELETE_TICKET action', () => {
+    expect(actions.deleteTicket(1)).toEqual({
+      type: 'DELETE_TICKET',
+      id: 1
+    });
+  });
+});
---
+++ src/actions/index.js
@@ -0,0 +1,2 @@
+export const deleteTicket = id => ({
+});
