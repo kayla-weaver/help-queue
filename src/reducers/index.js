b078a1d4f9ac6fd66efca51bafeacd4c271dbaf0
Kayla Weaver <kaylabeth1211@gmail.com>
2024-02-04 12:06:06 -0800

    green phase third reducer test

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,6 +3,8 @@ import ticketListReducer from './ticket-list-reducer';
 import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
+  formVisibleOnPage: formVisibleReducer,
+  mainTicketList: ticketListReducer
 });
 
 export default rootReducer;
