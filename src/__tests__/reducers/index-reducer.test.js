d00aedd470acf2318931f2ba5aa6853af6b44ca1
Kayla Weaver <kaylabeth1211@gmail.com>
2024-02-04 12:10:27 -0800

    red and green test four and five index reducer

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -21,4 +21,24 @@ describe("rootReducer", () => {
   test('Check that initial state of formVisibleReducer matches root reducer', () => {
     expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null }));
   });
+
+  test('Check that ADD_TICKET action works for ticketListReducer and root reducer', () => {
+    const action = {
+      type: 'ADD_TICKET',
+      names: 'Ryan & Aimen',
+      location: '4b',
+      issue: 'Redux action is not working correctly.',
+      id: 1
+    }
+    store.dispatch(action);
+    expect(store.getState().mainTicketList).toEqual(ticketListReducer(undefined, action));
+  });
+  
+  test('Check that TOGGLE_FORM action works for formVisibleReducer and root reducer', () => {
+    const action = {
+      type: 'TOGGLE_FORM'
+    }
+    store.dispatch(action);
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, action));
+  });
 });
