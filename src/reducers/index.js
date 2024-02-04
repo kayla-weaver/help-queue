f4df9bba798bd644a7052f0ab2fc9d36611e1a79
Kayla Weaver <kaylabeth1211@gmail.com>
2024-02-04 12:05:05 -0800

    red phase third reducer test

---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import formVisibleReducer from './form-visible-reducer';
+import ticketListReducer from './ticket-list-reducer';
+import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({
+});
+
+export default rootReducer;
