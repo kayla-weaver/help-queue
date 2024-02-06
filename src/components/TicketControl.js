f2e566696a1ad710d7f28f4f8aa424fbcc2bdeea
Kayla Weaver <kaylabeth1211@gmail.com>
2024-02-05 19:18:55 -0800

    adding action creators.

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -5,6 +5,7 @@ import TicketDetail from "./TicketDetail";
 import EditTicketForm from './EditTicketForm';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import * as a from './../actions';
 
 class TicketControl extends React.Component {
   constructor(props){
@@ -24,14 +25,7 @@ class TicketControl extends React.Component {
 
   handleEditingTicketInList = (ticketToEdit) => {
     const { dispatch } = this.props;
-    const { id, names, location, issue } = ticketToEdit;
-    const action = {
-      type: 'ADD_TICKET',
-      id: id,
-      names: names,
-      location: location,
-      issue: issue,
-    }
+    const action = a.addTicket(ticketToEdit);
     dispatch(action);
     this.setState({
       editing: false,
@@ -41,28 +35,16 @@ class TicketControl extends React.Component {
 
   handleAddingNewTicketToList = (newTicket) => {
     const { dispatch } = this.props;
-    const { id, names, location, issue } = newTicket;
-    const action = {
-      type: 'ADD_TICKET',
-      id: id,
-      names: names,
-      location: location,
-      issue: issue,
-    }
+    const action = a.addTicket(newTicket);
     dispatch(action);
-  const action2 = {
-    type: 'TOGGLE_FORM'
-  }
-  dispatch(action2);
+    const action2 = a.toggleForm();
+    dispatch(action2);
   }
 
 
   handleDeletingTicket = (id) => {
     const { dispatch } = this.props;
-    const action = {
-      type: 'DELETE_TICKET',
-      id: id
-    }
+    const action = a.deleteTicket(id);
     dispatch(action);
     this.setState({selectedTicket: null});
   }
@@ -76,9 +58,7 @@ class TicketControl extends React.Component {
       });
     } else {
       const { dispatch } = this.props;
-      const action = {
-        type: 'TOGGLE_FORM'
-      }
+      const action = a.toggleForm();
       dispatch(action);
     }
   }
