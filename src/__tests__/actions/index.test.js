484fe2f3bf5bd1c5271eaa618f5f1467cb334714
Kayla Weaver <kaylabeth1211@gmail.com>
2024-02-05 18:51:58 -0800

    red phase action index second test

--- src/__tests__/actions/index.test.js
+++ src/__tests__/actions/index.test.js
@@ -7,4 +7,10 @@ describe('Help Queue actions', () => {
       id: 1
     });
   });
+
+  it('toggleForm should create TOGGLE_FORM action', () => {
+    expect(actions.toggleForm()).toEqual({
+      type: 'TOGGLE_FORM'
+    });
+  });
 });
