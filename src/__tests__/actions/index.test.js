69ef5bdb1cc9abbb257831016528e1b45b2133c8
Kayla Weaver <kaylabeth1211@gmail.com>
2024-02-05 18:57:04 -0800

    red phase action index test three.

--- src/__tests__/actions/index.test.js
+++ src/__tests__/actions/index.test.js
@@ -13,4 +13,19 @@ describe('Help Queue actions', () => {
       type: 'TOGGLE_FORM'
     });
   });
+
+  it('addTicket should create ADD_TICKET action', () => {
+    expect(actions.addTicket({
+      names: 'Jo and Jasmine', 
+      location: '3E', 
+      issue: 'Redux not working!', 
+      id: 1
+    })).toEqual({
+      type: 'ADD_TICKET',
+      names: 'Jo and Jasmine',
+      location: '3E',
+      issue: 'Redux not working!',
+      id: 1
+    });
+  });
 });
